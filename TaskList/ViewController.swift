6e5d104569ec830552c23af306c561e75f508e89
vsvami <106614918+vsvami@users.noreply.github.com>
2024-04-04 16:51:54 +0400

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by Vladimir Dmitriev on 04.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
