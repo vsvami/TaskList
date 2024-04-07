6c548b8dab3e405d5dc40a4e3467c05b514728fd
vsvami <106614918+vsvami@users.noreply.github.com>
2024-04-07 20:18:50 +0400

    add logic with NewTaskViewController

--- TaskList/TaskListViewController.swift
+++ TaskList/TaskListViewController.swift
@@ -7,11 +7,86 @@
 
 import UIKit
 
+protocol NewTaskViewControllerDelegate: AnyObject {
+    func reloadData()
+}
+
 final class TaskListViewController: UITableViewController {
+    private var taskList: [ToDoTask] = []
+    private let cellID = "task"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setupNavigationBar()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        fetchData()
     }
 
+    @objc private func addNewTask() {
+        let newTaskVC = NewTaskViewController()
+        newTaskVC.delegate = self
+        present(newTaskVC, animated: true)
+    }
+    
+    private func fetchData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let fetchRequest = ToDoTask.fetchRequest()
+        
+        do {
+            taskList = try appDelegate.persistentContainer.viewContext.fetch(fetchRequest)
+        } catch {
+            print(error)
+        }
+    }
+}
+// MARK: - UITableViewDataSource
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+// MARK: - Setup UI
+private extension TaskListViewController {
+    func setupNavigationBar() {
+        title = "Task List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    
+        let navBarAppearance = UINavigationBarAppearance()
+        
+        navBarAppearance.backgroundColor = UIColor.milkBlue
+        
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        
+        // Add button to navigation bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
+}
+
+// MARK: - NewTaskViewControllerDelegate
+extension TaskListViewController: NewTaskViewControllerDelegate {
+    func reloadData() {
+        fetchData()
+        tableView.reloadData()
+    }
 }
